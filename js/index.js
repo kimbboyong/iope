44d40b5781dac64601f5cc644eee3d7a0c1c6d37
kimbboyong <kimbboyong@gmail.com>
2023-03-05 04:40:21 +0900

    80%

--- js/index.js
+++ js/index.js
@@ -14,28 +14,33 @@ $('.withiope li').hover(function () {
     }
 })
 
+
 // swiper
 var swiper = new Swiper(".comment .mySwiper", {
-
     pagination: {
         el: ".comment .swiper-pagination",
-        dynamicBullets: true,
     },
     effect: 'fade',
 });
 
 var swiper = new Swiper(".withiope .mySwiper", {
-    slidesPerView: 'auto',
-    freeMode: true,
-    spaceBetween: 15,
-    touchRatio: 0,
     loop: true,
-    pagination: {
-        el: ".withiope .swiper-pagination",
+    slidesPerView: 'auto',
+    slidesPerGroup: 3,
+    spaceBetween: 15,
+    breakpoints: { //반응형 조건 속성
+        640: { //640 이상일 경우
+            slidesPerGroup: 1, //레이아웃 2열
+        },
+        768: {
+            slidesPerGroup: 2,
+        },
+        1024: {
+            slidesPerGroup: 3,
+        },
     },
     navigation: {
         nextEl: ".withiope .swiper-button-next",
         prevEl: ".withiope .swiper-button-prev",
     },
-
 });
